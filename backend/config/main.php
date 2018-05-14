476d6396f4c0b3e41dbf9b46394e4f8bc6337414
Niran Wangyad <niran.w@cicnetgrp.net>
2018-05-14 08:20:24 +0700

    Use url prety

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,7 +10,7 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'defaultRoute'=>'dashboard',
+    //'defaultRoute'=>'dashboard',
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -95,6 +95,10 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
 
-        return $this->goHome();
+        //return $this->goHome();
+         $model = new LoginForm();
+         return $this->render('login', [
+                'model' => $model,
+            ]);
     }
 }
